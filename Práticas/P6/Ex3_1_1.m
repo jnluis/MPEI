3c9363133cae698d3977a5bcebc79eec92bb801c
jnluis <jnluis@ua.pt>
2022-11-08 20:32:47 +0000

    PL3-part 1

---
+++ Práticas/P6/Ex3_1_1.m
@@ -0,0 +1,51 @@
+% Exercício 1 da parte 2.3
+clear;clc;
+
+% 2 estados: faltar(F) ou não Faltar (NF)
+T= [0.2 0.3;
+    0.8 0.7];
+
+%Para verificar se é estocástica
+sum(T);
+(T >=0) & (T <=1);
+
+%% Alínea a
+%  Probablilidade de esta na aula da quarta seguinte,
+% sabendo que esteve na quarta
+v= [0 1]';
+
+v2 = T *v; % probabilidades de estar na sexta
+v3 = T *v2; % probabilidade de estar na quarta
+% Também podemos fazer isto a fazer T^2  ou seja o número de transiçoes a
+% levantar a T
+
+fprintf("Alinea A %f\n",v3(2))
+
+%% Alínea b
+%  Probablilidade de esta na aula da quarta seguinte,
+% sabendo que esteve na quarta
+v= [1 0]';
+
+v2 = T *v; % probabilidades de estar na sexta
+v3 = T *v2; % probabilidade de estar na quarta
+fprintf("Alinea B %0.4f\n",v3(2))
+
+%% Alínea c
+v= [0 1]';
+
+Vc = (T^29) * v;% é levantado a 29, porque a primeira aula já está definida
+fprintf("Alinea C %0.4f\n",v3(2))
+
+%% Alínea d
+n= 30; % numero de aulas
+v= [0.15 0.85]';
+VEF= zeros(1,n);
+VEF(1) = v(1);
+
+for i=1:(n-1)
+    v = T^i * v;
+    VEF(i+1) = v(1);
+end
+plot(1:n,VEF)
+axis([0 31 0.1 0.3])
+
---
+++ Práticas/P6/Ex3_1_2.m
@@ -0,0 +1,43 @@
+% Exercício 2 da parte 2.3
+clear;clc;
+
+% 2 estados: faltar(F) ou não Faltar (NF)
+T= [1/3 0.25 0;
+    1/3 11/20 0.5;
+    1/3 1/5 0.5];
+%% Alínea a
+%Para verificar se é estocástica
+sum(T);
+(T >=0) & (T <=1);
+% Se a matriz der só 1, é porque é estocástica
+
+%% Alínea B
+n= 90; % nº de alunos
+v = [60 15 15]' /n;
+
+%% Alínea C
+Vc = (T^30) *v;
+format short
+
+NalunosA= Vc(1) *90;
+NalunosB= Vc(2) *90;
+NalunosC= Vc(3) *90;
+
+fprintf("Grupo A %0.0f\n",NalunosA)
+fprintf("Grupo B %0.0f\n",NalunosB)
+fprintf("Grupo C %0.0f\n",NalunosC)
+
+%% Alínea D
+n= 90; % nº de alunos
+v = [30 30 30]' /n;
+
+Vc = (T^30) *v;
+format short
+
+NalunosA= Vc(1) *90;
+NalunosB= Vc(2) *90;
+NalunosC= Vc(3) *90;
+
+fprintf("Grupo A com %0.0f\n",NalunosA)
+fprintf("Grupo B %0.0f\n",NalunosB)
+fprintf("Grupo C %0.0f\n",NalunosC)
